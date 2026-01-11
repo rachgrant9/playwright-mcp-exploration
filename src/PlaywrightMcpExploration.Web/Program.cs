85ad31e2d029dc147d2e672932da259c4cd1d8a7
Rachel Grant <rachelgrant09@gmail.com>
2026-01-11 09:49:04 +0000

    feat: register DB service

--- src/PlaywrightMcpExploration.Web/Program.cs
+++ src/PlaywrightMcpExploration.Web/Program.cs
@@ -1,5 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using PlaywrightMcpExploration.Web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Register DbContext with SQLite in-memory database
+builder.Services.AddDbContext<TodoDbContext>(options =>
+    options.UseSqlite("DataSource=:memory:"));
+
+// Register repository
+builder.Services.AddScoped<ITodoRepository, TodoRepository>();
+
 var app = builder.Build();
+
+// Ensure database is created on startup
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<TodoDbContext>();
+    dbContext.Database.OpenConnection();
+    dbContext.Database.EnsureCreated();
+}
 
 app.MapGet("/", () => "Hello World!");
 
